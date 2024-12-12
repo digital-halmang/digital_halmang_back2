8f58810d5d9cdb1a5261f6e6e706409dac6ac028
jeongyeonho <fnelclsrn123@gmail.com>
2024-12-12 21:25:24 +0900

    feat: test용 api 생성

---
+++ src/main/java/com/example/kakao/domain/index/IndexController.java
@@ -0,0 +1,14 @@
+package com.example.kakao.domain.index;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Server is running");
+    }
+}
