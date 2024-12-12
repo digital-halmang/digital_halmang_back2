32b2ee5903a5dbb1ad6afd75c4a79d45a2f67100
qwas7031 <qwas7031@naver.com>
2024-12-12 18:26:46 +0000

    d

--- src/main/java/com/example/kakao/domain/sts/controller/SttController.java
+++ src/main/java/com/example/kakao/domain/sts/controller/SttController.java
@@ -1,18 +1,14 @@
 package com.example.kakao.domain.sts.controller;
 
-import com.example.kakao.domain.claude.model.response.ContentItem;
 import com.example.kakao.domain.claude.model.response.Contents;
-import com.example.kakao.domain.sts.service.SttService;
+import com.example.kakao.domain.claude.service.ClaudeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-import java.util.*;
-
 import java.io.IOException;
 import java.util.List;
+import java.util.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,7 +20,7 @@ public class SttController {
     @PostMapping(value = "/api/audio", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public Contents handleAudioMessage() throws IOException {
 		System.out.println("@@@@@@");
-		service.getContents("떡볶이는 맛있어 내 최애 음식이야");
+		return service.getContents("떡볶이는 맛있어 내 최애 음식이야");
 		
 		
 
