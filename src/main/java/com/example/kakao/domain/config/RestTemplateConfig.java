63b78d08bc81d5949ba0500a904a3ecc3278bdaa
qwas7031 <qwas7031@naver.com>
2024-12-12 19:35:34 +0000

    저장

--- src/main/java/com/example/kakao/domain/config/RestTemplateConfig.java
+++ src/main/java/com/example/kakao/domain/config/RestTemplateConfig.java
@@ -2,14 +2,26 @@ package com.example.kakao.domain.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+
+        // 프록시 설정
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("krmp-proxy.9rum.cc", 3128));
+        factory.setProxy(proxy);
+
+        // 타임아웃 설정
+        factory.setConnectTimeout(5000);
+        factory.setReadTimeout(5000);
+        return new RestTemplate(factory);
     }
 }
-
