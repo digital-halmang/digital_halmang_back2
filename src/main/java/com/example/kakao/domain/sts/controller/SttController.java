249546adb60a59ce4768f758052fba95ad9a9d62
qwas7031 <qwas7031@naver.com>
2024-12-12 18:39:06 +0000

    d

--- src/main/java/com/example/kakao/domain/sts/controller/SttController.java
+++ src/main/java/com/example/kakao/domain/sts/controller/SttController.java
@@ -1,11 +1,11 @@
 package com.example.kakao.domain.sts.controller;
 
+
 import com.example.kakao.domain.claude.model.response.Contents;
 import com.example.kakao.domain.claude.service.ClaudeService;
 import com.example.kakao.domain.sts.service.SttService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
@@ -17,12 +17,9 @@ public class SttController {
     private final SttService sttService;
     private final ClaudeService service;
 
-    @PostMapping(value = "/api/audio", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @GetMapping("/aa")
     public Contents handleAudioMessage() throws IOException {
         System.out.println("@@@@@@");
         return service.getContents("떡볶이는 맛있어 내 최애 음식이야");
-
-
-
     }
 }
