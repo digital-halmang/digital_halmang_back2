a4c2983849e0ce7e4619710e150e233d46cbf6e1
qwas7031 <qwas7031@naver.com>
2024-12-12 18:00:33 +0000

    fix: add module

--- src/main/java/com/example/kakao/domain/sts/controller/SttController.java
+++ src/main/java/com/example/kakao/domain/sts/controller/SttController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import java.util.*;
 
 import java.io.IOException;
 import java.util.List;
