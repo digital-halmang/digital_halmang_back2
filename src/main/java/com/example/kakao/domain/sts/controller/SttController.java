1c7124e4b13f10966538b65612bc3cfdcc8c10df
qwas7031 <qwas7031@naver.com>
2024-12-12 22:14:02 +0000

    d

--- src/main/java/com/example/kakao/domain/sts/controller/SttController.java
+++ src/main/java/com/example/kakao/domain/sts/controller/SttController.java
@@ -5,10 +5,16 @@ import com.example.kakao.domain.claude.model.response.BlogPost;
 import com.example.kakao.domain.claude.model.response.Caption;
 import com.example.kakao.domain.claude.model.response.CardNews;
 import com.example.kakao.domain.claude.service.ClaudeService;
+import com.example.kakao.domain.sts.service.SttService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 
 @RequiredArgsConstructor
@@ -16,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SttController {
 
     private final ClaudeService service;
+    private final SttService sttService;
 
     @PostMapping("/api/cardnews")
     public CardNews getCardNews(@RequestBody String text) {
@@ -30,5 +37,10 @@ public class SttController {
     @PostMapping("/api/caption")
     public Caption getCaption(@RequestBody String text) {
         return service.getCaption(text);
+    }
+
+    @PostMapping(value = "/api/audio", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String handleAudioMessage(@RequestParam("audioFile") MultipartFile audioFile) throws IOException {
+        return sttService.transcribe(audioFile, 44100);
     }
 }
